cfbcb9fb3a444e8be209ade178ca78797d7a5a43
Arjunslab <166507445+Arjunslab@users.noreply.github.com>
2025-06-10 17:51:31 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NerdKillBot from './NerdKillBot';
+
+function App() {
+  return (
+    <main className="min-h-screen bg-black text-white p-6">
+      <NerdKillBot />
+    </main>
+  );
+}
+
+export default App;
